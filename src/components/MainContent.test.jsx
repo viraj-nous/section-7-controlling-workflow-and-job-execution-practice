bdfbd9e4693f991bdf4727180dd806de672f5501
Viraj Bafana (BLR GSS) <virajb@nousinfo.com>
2025-05-27 11:04:43 +0530

    fix the test

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -11,7 +11,7 @@ describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
 
-    expect(screen.getByRole('navigation')).toBeInTheDocument();
+    expect(screen.getByRole('button')).toBeInTheDocument();
   });
 
   it('should show the help area after clicking the button', async () => {
