b6d6b632d8408f812328c7ecec741f7d3005f621
Viraj Bafana (BLR GSS) <virajb@nousinfo.com>
2025-05-27 10:31:26 +0530

    broke test

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -11,7 +11,7 @@ describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
 
-    expect(screen.getByRole('button')).toBeInTheDocument();
+    expect(screen.getByRole('navigation')).toBeInTheDocument();
   });
 
   it('should show the help area after clicking the button', async () => {
